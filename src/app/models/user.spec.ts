4ff364cbee5c24f64ee51124d72ac8a3440e20ea
USER <helasoui>
2021-01-06 12:16:03 +0100

    binding data to a Model

---
+++ src/app/models/user.spec.ts
@@ -0,0 +1,7 @@
+import { USer } from './user';
+
+describe('USer', () => {
+  it('should create an instance', () => {
+    expect(new USer()).toBeTruthy();
+  });
+});
---
+++ src/app/models/user.ts
@@ -0,0 +1,10 @@
+export class USer {
+    constructor(public name: string,
+        public email: string,
+        public phone : number,
+        public topic : string,
+        public time:string,
+        public subscribe : boolean){
+
+    }
+}
